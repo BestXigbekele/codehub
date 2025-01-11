d068f90660992b7e4bea33214f9e0b32080438d4
Topclass <145531034+BestXigbekele@users.noreply.github.com>
2025-01-11 03:44:59 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+
+/*function open_close(){
+
+    var SIDEBAR = document.getElementById('sidebar');
+    var MENUBUTTON = document.getElementById('menubutton');
+    var DATA_SIDE = document.getElementById('data-main');
+    if (SIDEBAR.style.width == '50px'){
+        SIDEBAR.style.width = '250px';
+        DATA_SIDE.style.marginLeft = '250px';
+    }else{
+        SIDEBAR.style.width = '50px';
+    DATA_SIDE.style.marginLeft = '50px';
+    }
+    
+}*/
